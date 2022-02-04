366cc82987dad0a7e9b96fba199f951495733fa2
Jaredeco <jano.redecha@gmail.com>
2022-02-04 16:21:49 +0100

    Updated hosting specific files

--- lib/widgets/components/landing/social_icon.dart
+++ lib/widgets/components/landing/social_icon.dart
@@ -2,26 +2,30 @@ import 'package:flutter/material.dart';
 
 class SocialIcon extends StatelessWidget {
   final IconData? icon;
-  const SocialIcon({Key? key, this.icon}) : super(key: key);
+  final VoidCallback? onTap;
+  const SocialIcon({Key? key, this.icon, this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 50,
-      height: 50,
-      margin: const EdgeInsets.only(left: 20),
-      decoration: const BoxDecoration(
-          color: Colors.white,
-          shape: BoxShape.circle,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              offset: Offset(0, 10),
-              blurRadius: 10,
-            )
-          ]),
-      child: Center(
-        child: Icon(icon, color: const Color(0xFFD1DBEA)),
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: 50,
+        height: 50,
+        margin: const EdgeInsets.only(left: 20),
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0, 10),
+                blurRadius: 10,
+              )
+            ]),
+        child: Center(
+          child: Icon(icon, color: const Color(0xFFD1DBEA)),
+        ),
       ),
     );
   }
