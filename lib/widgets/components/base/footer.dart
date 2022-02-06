8c238fe468bf6896bffc6a43e604912433d2dec8
Jaredeco <jano.redecha@gmail.com>
2022-02-06 19:55:01 +0100

    Updated footer

--- lib/widgets/components/base/footer.dart
+++ lib/widgets/components/base/footer.dart
@@ -12,81 +12,122 @@ class Footer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 50.0),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.5,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            MediaQuery.of(context).size.width >= largePageSize
-                ? const GradientText(
-                    text: "Kontaktujte Nás!",
-                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                  )
-                : const CustomText(
-                    textAlign: TextAlign.center,
-                    text: "Kontaktujte Nás!",
-                    size: 40,
-                    color: Color(0xFF45E994),
-                    weight: FontWeight.bold),
-            const SizedBox(
-              height: 20,
+      padding: const EdgeInsets.only(top: 50.0, bottom: 50),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          MediaQuery.of(context).size.width >= largePageSize
+              ? const GradientText(
+                  text: "Kontaktujte Nás!",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                )
+              : const CustomText(
+                  textAlign: TextAlign.center,
+                  text: "Kontaktujte Nás!",
+                  size: 40,
+                  color: Color(0xFF45E994),
+                  weight: FontWeight.bold),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(
+                Icons.email,
+                color: Color(0xFFb0BFDE),
+              ),
+              CustomText(
+                padding: EdgeInsets.only(left: 10),
+                text: "vego.egmt@gmail.com",
+                color: Color(0xFFb0BFDE),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ButtonDown(
+            icon: FontAwesomeIcons.instagram,
+            size: 25,
+            color: const Color(0xFF45E994),
+            onTap: () async {
+              await launch("https://www.instagram.com/vegoegmt/");
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(
+                Icons.copyright,
+                color: Color(0xFFb0BFDE),
+              ),
+              CustomText(
+                padding: EdgeInsets.only(left: 10),
+                text: "2022 VEGO Š.F.",
+                color: Color(0xFFb0BFDE),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: const TextSpan(
+                style: TextStyle(
+                    fontSize: 16.0,
+                    color: Color(0xFFb0BFDE),
+                    fontFamily: 'Sora'),
+                children: <TextSpan>[
+                  TextSpan(text: 'Designed and Developed by '),
+                  TextSpan(
+                      text: ' Ján Redecha',
+                      style: TextStyle(
+                          color: Color(0xFF45E994), fontFamily: 'Sora')),
+                ],
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Icon(
-                  Icons.email,
-                  color: Color(0xFFb0BFDE),
-                ),
-                CustomText(
-                  padding: EdgeInsets.only(left: 10),
-                  text: "vego.egmt@gmail.com",
-                  color: Color(0xFFb0BFDE),
-                ),
-              ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: const TextSpan(
+                style: TextStyle(
+                    fontSize: 16.0,
+                    color: Color(0xFFb0BFDE),
+                    fontFamily: 'Sora'),
+                children: <TextSpan>[
+                  TextSpan(text: 'Like my Work? Contact me at'),
+                  TextSpan(
+                      text: ' jano.redecha@gmail.com',
+                      style: TextStyle(
+                          color: Color(0xFF45E994), fontFamily: 'Sora')),
+                ],
+              ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            ButtonDown(
-              icon: FontAwesomeIcons.instagram,
-              size: 25,
-              color: const Color(0xFF45E994),
-              onTap: () async {
-                await launch("https://www.instagram.com/vegoegmt/");
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Icon(
-                  Icons.copyright,
-                  color: Color(0xFFb0BFDE),
-                ),
-                CustomText(
-                  padding: EdgeInsets.only(left: 10),
-                  text: "Developed and Designed by Ján Redecha.",
-                  color: Color(0xFFb0BFDE),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            InkWell(
-              onTap: () => Navigator.of(context).pushNamed("/"),
-              child: SizedBox(
-                  height: 90,
-                  width: 150,
-                  child: Image.asset("assets/images/logo_vego.png")),
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: () => Navigator.of(context).pushNamed("/"),
+            child: SizedBox(
+                height: 90,
+                width: 150,
+                child: Image.asset("assets/images/logo_vego.png")),
+          ),
+        ],
       ),
     );
   }
