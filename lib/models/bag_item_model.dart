bb8b9477ac04cc5606cfac2ac3649b62aab91dd6
Jaredeco <jano.redecha@gmail.com>
2022-02-06 14:50:39 +0100

    Updated admin panel for option handling

--- lib/models/bag_item_model.dart
+++ lib/models/bag_item_model.dart
@@ -4,17 +4,21 @@ class BagItemModel {
   final String? id;
   final String? productId;
   final String? productQuantity;
+  final String? productOption;
 
   BagItemModel({
     this.id,
     this.productId,
     this.productQuantity,
+    this.productOption
   });
 
   factory BagItemModel.fromDocSnapshot(DocumentSnapshot snapshot) {
     return BagItemModel(
         id: snapshot.id,
         productId: snapshot["productId"],
-        productQuantity: snapshot["productQuantity"]);
+        productQuantity: snapshot["productQuantity"], 
+        productOption: snapshot["productOption"]
+        );
   }
 }
