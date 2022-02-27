5dbd7e7083d687e873f828ad89d83d53377dfbc0
Jaredeco <jano.redecha@gmail.com>
2022-02-27 10:31:35 +0100

    Updated functionality of admin panel

--- lib/screens/main/base/responsive_ui.dart
+++ lib/screens/main/base/responsive_ui.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'base_large.dart';
 import 'base_small.dart';
 
-const int largePageSize = 200;
+const int largePageSize = 50;
 
 class ResponsiveUi extends StatelessWidget {
   final List<Widget>? smallWidgets;
